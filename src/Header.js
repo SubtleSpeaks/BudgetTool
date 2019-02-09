9c7019cd54e465bfc93d637e8c3b50291ba6b77d
Seb Ringrose <sebringrose@gmail.com>
2019-02-09 14:49:50 +0000

    adjusted font size

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,7 @@ class Header extends Component {
           <img src={logo} className="App-logo" alt="logo" width="120"/>
         </div>
         <div className="App-header-center">
-          <h1>🧮 Student Budget Tool 💸</h1>
+          <h1>🧮 Student Budget Calculator 💸</h1>
         </div>
         <div className="App-header-right">
         </div>
